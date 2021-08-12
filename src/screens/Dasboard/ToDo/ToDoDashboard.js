e2200e89ab2d0c7d797b01667bcc8462b36ba59e
chrizu <chrizuuus@wp.pl>
2021-08-12 14:11:38 +0200

    Add simple input form for add Task

--- src/screens/Dasboard/ToDo/ToDoDashboard.js
+++ src/screens/Dasboard/ToDo/ToDoDashboard.js
@@ -1,23 +1,41 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text, StatusBar, FlatList, View, TouchableOpacity} from 'react-native';
+import {SafeAreaView, Text, StatusBar, FlatList, View, TouchableOpacity,TextInput,Keyboard} from 'react-native';
 import FlexLayout from '../../../components/Layouts/FlexLayout'
 import realm, { createTask, getAllTasks } from "../../../components/Helpers/Database"
 
 
 const ToDoDashboad = () => {
     const [tasks, setTask] = useState(getAllTasks());
+    const [input,setInput] = useState()
+
+    const submitHandler = (value) => {
+        createTask(value.nativeEvent.text)
+        setTask(getAllTasks())
+        Keyboard.dismiss
+        setInput('')
+    }
+
+    const changeHandler = (value) => {
+        setInput(value)
+    }
 
     return (
     <>
     <FlexLayout>
             <Text 
-                style={{paddingVertical: 8}}
-                onPress={() => {
-                createTask("Chronicles of JavaScript")
-                setTask(getAllTasks())
-            }}>
+                style={{paddingVertical: 8}}>
                 Add task
             </Text> 
+
+            <TextInput 
+                style={{borderColor: 'black', borderWidth: 1}}
+                name="setTask"
+                onChangeText = {(input) => changeHandler(input)}
+                value={input}
+                onSubmitEditing={(event) => {
+                    submitHandler(event)
+                }}
+            />
 
             <FlatList
                 data={tasks}
@@ -27,7 +45,7 @@ const ToDoDashboad = () => {
                 <FlexLayout>
                     <Text>{item.title}</Text>
                     <Text>{item.isDone.toString()}</Text>
-                    <Text>{item.createdDate}</Text>
+                    <Text>{item.createdDate.toString()}</Text>
                 </FlexLayout>
                 )
             }} />
